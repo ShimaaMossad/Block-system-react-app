852b1939577239b0cbf5d173cd3b1dd675e8c8a2
ShimaaMossad <shimaa.mossad.elbaz@gmail.com>
2025-06-16 06:09:23 +0300

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,1 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
+app.use('/api/posts', postRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected!');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('DB connection error:', err);
+  });
